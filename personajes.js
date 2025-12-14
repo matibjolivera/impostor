082baf955d7847c660eb03e49953301f9bb26d22
matibjolivera <matibjolivera@gmail.com>
2025-12-14 16:37:07 -0300

    game

---
+++ personajes.js
@@ -0,0 +1,18 @@
+window.PERSONAJES = {
+  "Boca Juniors - Históricos": [
+    "Martín Palermo","Juan Román Riquelme","Carlos Tevez","Guillermo Barros Schelotto",
+    "Óscar Córdoba","Hugo Ibarra","Sebastián Battaglia","Diego Cagna",
+    "Mauricio Serna","Jorge Bermúdez","Cristian Traverso","Roberto Abbondanzieri",
+    "Marcelo Delgado","Alfredo Graciani","Claudio Morel Rodríguez",
+    "Héctor Bracamonte","Pablo Ledesma","Daniel Díaz","Rolando Schiavi",
+    "Walter Samuel","Nicolás Burdisso","Fernando Gago"
+  ],
+
+  "Selección Argentina - Recientes": [
+    "Lionel Messi","Ángel Di María","Emiliano Martínez","Enzo Fernández",
+    "Julián Álvarez","Rodrigo De Paul","Cristian Romero","Alexis Mac Allister",
+    "Nahuel Molina","Gonzalo Montiel","Lisandro Martínez","Leandro Paredes",
+    "Giovani Lo Celso","Lautaro Martínez","Nicolás Otamendi","Marcos Acuña",
+    "Nicolás Tagliafico","Paulo Dybala"
+  ],
+};
